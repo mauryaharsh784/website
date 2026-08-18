b516a6c197f40d253bf35d87a47043b7d0eb2dcc
Harsh Vardhan Maurya <mauryaharsh784@gmail.com>
2026-08-19 01:32:27 +0530

    Delete gram-panchayat-site (1) directory

--- gram-panchayat-site (1)/app/src/components/ui/RoadDivider.jsx
+++
@@ -1,7 +0,0 @@
-export default function RoadDivider({ className = "" }) {
-  return (
-    <div className={`mx-auto flex h-16 w-px flex-col items-center ${className}`}>
-      <div className="road-stitch h-full" />
-    </div>
-  );
-}
